dd4070124fd56356cfb247934d6d898c62ece457
Aleksandr Repeckiy <repetskiyaleksandr@gmail.com>
2021-05-21 19:41:39 +0300

    add vue skills

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,43 @@
-console.log('this is the skills module');
+import Vue from 'vue';
+
+const skillsItem = {
+	props: ['skill'],
+	template: "#skills-item",
+	methods: {
+		drawPercentedCircle() {
+			const circle = this.$refs["colored_circle"];
+			const dashArray = parseInt(
+				getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+			);
+			const percent = dashArray / 100 * (100 - this.skill.percent)
+			circle.style.strokeDashoffset = percent;
+		}
+	},
+	mounted() {
+		this.drawPercentedCircle();
+	}
+}
+
+const skillsRow = {
+	props: ['category'],
+	template: "#skills-row",
+	components: {
+		skillsItem: skillsItem
+	}
+}
+
+new Vue ({
+	el: "#skills-component",
+	template: "#skills-list ",
+	components: {
+		skillsRow: skillsRow
+	},
+	data() {
+		return {
+			skills: []
+		}
+	},
+	created() {
+		this.skills = require("../data/skills.json");
+	},
+})
